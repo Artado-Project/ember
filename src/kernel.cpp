c4a958eb7ebb9471ad50048d659d39063d1d0024
Camroku <cinaryilmaz.gnu@gmail.com>
2022-06-05 19:38:52 +0300

    Some fixes

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -13,12 +13,10 @@ extern "C" void kernel_main(void)
   terminal::setcolor(VGA_COLOR_LIGHT_BROWN, VGA_COLOR_BLACK);
   terminal::print("   __ _ _ __ _   _ ___ \n"
                   "  / _` | '__| | | / __|\n"
-                  " | (_| | |  | |_| \__ \ \n"
-                  "  \__,_|_|   \__,_|___/ \n");
+                  " | (_| | |  | |_| \\__ \\ \n"
+                  "  \\__,_|_|   \\__,_|___/ \n");
 
   terminal::setcolor(VGA_COLOR_WHITE, VGA_COLOR_BLACK);
   terminal::print("Welcome to Arus!\n");
-
-  terminal::setcolor(VGA_COLOR_WHITE, VGA_COLOR_BLACK);
   terminal::print("Version 1, Build 0.0.1\n");
 }
