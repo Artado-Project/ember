b7a62f7eac93c4ef43437d3cd616cec4005768e0
Camroku <cinaryilmaz.gnu@gmail.com>
2022-06-12 23:34:23 +0300

    Added sti after welcome text, gives error

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -20,6 +20,9 @@ extern "C" void kernel_main(void)
   terminal::setcolor(VGA_COLOR_WHITE, VGA_COLOR_BLACK);
   terminal::print("Welcome to Arus!\n");
   terminal::print("Version 1, Build 0.0.1\n");
+  
+  asm volatile("sti");
+
   asm volatile ("int $0x3");
   asm volatile ("int $0x4");
   init_timer(50);
